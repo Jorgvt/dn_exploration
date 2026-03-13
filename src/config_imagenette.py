0a432278e4268a6f85b78a71a29c21d807ce9c75
Jorgvt <vila_tronik@hotmail.com>
2026-03-13 13:48:53 +0100

    moving along to a package

---
+++ src/config_imagenette.py
@@ -0,0 +1,9 @@
+from ml_collections import ConfigDict
+
+config = ConfigDict({
+        "EPOCHS": 500,
+        "SEED": 42,
+        "BATCH_SIZE": 64,
+        "LEARNING_RATE": 1e-3,
+        "GAP": True,
+        })
